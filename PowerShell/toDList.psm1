6c9672320ef3c1f7a2e6a56143defed87bb80b40
Ian Simons <iansimons93@gmail.com>
2024-11-18 11:05:11 -0500

    removing personalization

--- PowerShell/toDList.psm1
+++ PowerShell/toDList.psm1
@@ -1,19 +1,19 @@
-new-Item C:\Users\ianms\toDoList.txt
-attrib +h C:\Users\ianms\toDoList.txt
+new-Item C:\Users\$Env:UserName\toDoList.txt
+attrib +h C:\Users\$Env:UserName\toDoList.txt
 
 function Add-ToDo{
     param ([string]$newTask)
-    echo $newTask >> C:\Users\ianms\toDoList.txt
+    echo $newTask >> C:\Users\$Env:UserName\toDoList.txt
 }
 
 function Print-ToDo{
-    Get-Content -Path C:\Users\ianms\toDoList.txt
+    Get-Content -Path C:\Users\$Env:UserName\toDoList.txt
 }
 
 function Complete-TaskByName{
     param([string]$completedTask)
-    attrib -h C:\Users\ianms\toDoList.txt
-    $todoList = Get-Content -Path C:\Users\ianms\toDoList.txt
+    attrib -h C:\Users\$Env:UserName\toDoList.txt
+    $todoList = Get-Content -Path C:\Users\$Env:UserName\toDoList.txt
     $taskFound = $false
     $newList = @()
     if($todoList -is [array]){
@@ -37,21 +37,21 @@ function Complete-TaskByName{
             write-host $todoList
         }
         else{
-            remove-item C:\Users\ianms\toDoList.txt
+            remove-item C:\Users\$Env:UserName\toDoList.txt
             for ($i = 0; $i -lt $newList.Length; $i++){
-                $newList[$i] >> C:\Users\ianms\toDOList.txt
+                $newList[$i] >> C:\Users\$Env:UserName\toDOList.txt
             }
         }
         if ($newList.Length -eq 0){
-            new-Item C:\Users\ianms\toDoList.txt | out-null
+            new-Item C:\Users\$Env:UserName\toDoList.txt | out-null
         }
-        attrib +h C:\Users\ianms\toDoList.txt
+        attrib +h C:\Users\$Env:UserName\toDoList.txt
     }
     else{
         if ($completedTask -eq $todoList){
-            remove-Item C:\Users\ianms\toDoList.txt | out-null
-            new-Item C:\Users\ianms\toDoList.txt | out-null
-            attrib +h C:\Users\ianms\toDoList.txt
+            remove-Item C:\Users\$Env:UserName\toDoList.txt | out-null
+            new-Item C:\Users\$Env:UserName\toDoList.txt | out-null
+            attrib +h C:\Users\$Env:UserName\toDoList.txt
         }
         else{
             write-host 'task not found, existing task is: '
