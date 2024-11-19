ee5afceb8edc00bce0f86c05de3a86489ce0ac8e
Ian Simons <iansimons93@gmail.com>
2024-11-19 09:15:01 -0500

    update to classmate's project

--- PowerShell/uptime.ps1
+++ PowerShell/uptime.ps1
@@ -10,11 +10,23 @@ while ($true){
 		[string]$response = ping $currentService
 		if ($response -match "Destination host unreachable"){
 			write-host "$currentService appears to be down"
+			write-host "Press enter to dismiss alarm"
+			while($true){
+				if ([Console]::KeyAvailable){
+					$key = [Console]::ReadKey($true)
+					if ($key.Key -eq "Enter"){
+						break
+					}
+				}
+				[console]::Beep(1000,500)
+			}
+			write-host "Alarm dismissed, please fix issue in order to prevent alarm from triggering again"
 		}
+
 		else{
 			write-host "$currentService is up"
 		}
-
-	}	
+	}
+	Start-Sleep -Seconds 10	
 }
 
