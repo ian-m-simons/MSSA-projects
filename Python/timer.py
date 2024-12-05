ba61dfe7fd20e97181eb1759ba0f770bd0ea820a
Ian Simons <iansimons93@gmail.com>
2024-12-05 13:33:42 -0500

    update

--- Python/timer.py
+++ Python/timer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import time
 
 def inputInt(prompt):
@@ -13,12 +14,30 @@ def inputInt(prompt):
 
 def main():
     timer = inputInt("Please enter time in seconds ")
-#todo change timer so it uses the seconds from date time rather than sleep
-#this will make it more accurate
+#todo currently doesn't delete last digit when no longer needed
+    usefultime = datetime.now()
+    savedtime = (usefultime.second*1000000) +usefultime.microsecond
+    firstRun = True
     while timer > 0:
-        print(timer, end='\r')
-        timer -= 1
-        time.sleep(1)
+        ct = datetime.now()
+        currenttime = (ct.second*1000000)+ct.microsecond
+        if firstRun:
+            print(timer, end='\r')
+            timer -= 1
+            firstRun = False
+        if currenttime >= savedtime + 1000000:
+            if timer >1:
+                print(timer, end='\r')
+                timer -= 1
+                usefultime = datetime.now()
+                savedtime = (usefultime.second *1000000) + usefultime.microsecond
+            else:
+                print(timer)
+                timer -= 1
+                time.sleep(1)
+        else:
+            pass
+        
     print("Timer has expired")
 
 if __name__ == "__main__":
